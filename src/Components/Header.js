de35175c0eac3b33f553530a836695993eced6d4
Jason Welsh <jasonbwelsh@gmail.com>
2018-05-28 12:15:14 -0400

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Nav from './Nav';
+
+class Header extends Component {
+
+  doAThing() {
+    console.log('I did a thing');
+  }
+
+  render() {
+    return (
+      <header onClick={this.doAThing}>
+        <h1>Hi</h1>
+        <Nav />
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/Components/Nav.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+  render() {
+    const pages = ['home', 'blog', 'pics', 'bio', 'art', 'shop', 'about', 'contact'];
+    const navLinks = pages.map( page => {
+      return (
+        <a href={'/' + page}>
+          {page}
+        </a>
+      )
+    });
+  }
+  return <nav>{navLinks}</nav>;
+}
+
+export default Nav;
