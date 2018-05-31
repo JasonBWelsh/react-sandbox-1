cea691d52c9c851255b7f93b9ddbb4dd8198008c
Jason Welsh <jasonbwelsh@gmail.com>
2018-05-31 12:42:19 -0400

    state updated, wrapper function to call setState() added and binded this in contructor

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import { Main } from './Components/Main';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentNum : 3,
+      currentLetter : 'z',
+      isTrue: true
+    };
+    this.toggleTruth = this.toggleTruth.bind(this);
+  }
+
+  // function to wrap setState call
+  toggleTruth() {
+    const truth = this.state.isTrue === true ? false : true;
+    this.setState({
+      isTrue : truth
+    });
+  }
+
   render() {
     return (
       <div className="App">
